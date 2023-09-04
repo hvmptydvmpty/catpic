06d80cc8b4b69823706d518cdef235d0e439961d
Sergey Lipnevich <sergey@optimaltec.com>
2023-09-04 18:49:59 -0400

    optional guessing of image type from content rather than file name (~46% slower); counting as a structure

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,109 @@
 use async_std::stream::StreamExt;
 use async_std::{fs, path, task};
+use image;
+use memmap2;
 
-async fn walk<P: AsRef<path::Path>>(p: P, verbose: bool) -> std::io::Result<()> {
+#[derive(Debug)]
+struct Count {
+    dir: u64,
+    img: u64,
+    oth: u64,
+}
+
+impl Count {
+    fn zero() -> Self {
+        Self {
+            dir: 0,
+            img: 0,
+            oth: 0,
+        }
+    }
+}
+
+impl std::fmt::Display for Count {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "directory(ies): {}, image(s): {}, other file(s): {}",
+            self.dir, self.img, self.oth
+        )
+    }
+}
+
+async fn open_image<P: AsRef<path::Path>>(
+    p: P,
+    guess: bool,
+) -> image::ImageResult<image::DynamicImage> {
+    if guess {
+        let file = fs::File::open(p).await?;
+        let bytes = unsafe { memmap2::Mmap::map(&file)? };
+        let reader = image::io::Reader::new(std::io::Cursor::new(bytes)).with_guessed_format()?;
+        reader.decode()
+    } else {
+        image::open(p.as_ref())
+    }
+}
+
+async fn walk<P: AsRef<path::Path>>(
+    p: P,
+    guess: bool,
+    verbose: bool,
+    summary: bool,
+) -> std::io::Result<()> {
     let mut worq = std::collections::VecDeque::from([p.as_ref().to_path_buf()]);
-    let mut dc = 0;
-    let mut fc = 0;
+    let mut count = Count::zero();
 
     while !worq.is_empty() {
         let current = worq.pop_front().unwrap();
-        dc += 1;
-        if verbose {
-            println!("{}: traversing", current.display());
-        }
-        let mut entries = fs::read_dir(current).await?;
+        let mut local_count = Count::zero();
+        let mut entries = fs::read_dir(&current).await?;
 
+        count.dir += 1;
         while let Some(res) = entries.next().await {
             let entry = res?;
             if entry.file_type().await?.is_dir() {
                 worq.push_back(entry.path());
+                local_count.dir += 1;
             } else {
-                fc += 1;
-                if verbose {
-                    println!("\t{}", entry.path().display());
+                match open_image(path::Path::new(&entry.path()), guess).await {
+                    Ok(pic) => {
+                        local_count.img += 1;
+                        if verbose {
+                            println!("\t{} {:?}", entry.path().display(), pic.color());
+                        }
+                    }
+                    Err(error) => {
+                        local_count.oth += 1;
+                        if verbose {
+                            println!("\t{} ignored: {:?}", entry.path().display(), error);
+                        }
+                    }
                 }
             }
         }
+
+        // don't add local_count.dir here, we count them separately when walking
+        count.img += local_count.img;
+        count.oth += local_count.oth;
+        if verbose {
+            println!("{} has {}", current.display(), local_count);
+        }
     }
-    if verbose {
-        println!("Directory(ies): {dc}, file(s): {fc}");
+    if summary {
+        println!("Total {}", count);
     }
     Ok(())
 }
 
 fn main() {
+    let mut guess = false;
     let mut verbose = false;
     for arg in std::env::args() {
         match arg.to_lowercase().as_ref() {
+            "--guess" | "-g" => guess = true,
             "--verbose" | "-v" => verbose = true,
             _ => (),
         }
     }
-    let _ = task::block_on(walk(".", verbose));
+    let _ = task::block_on(walk(".", guess, verbose, true));
 }
